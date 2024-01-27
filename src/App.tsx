03c6fa5a3c88f11add2375f1e9c42629f1a2c912
Gihoon Lim <knv429@naver.com>
2024-01-27 13:24:13 +0900

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,141 @@
 import styled from "styled-components";
-import { Variants, motion } from "framer-motion";
-import { useRef } from "react";
+import { AnimatePresence, Variants, motion } from "framer-motion";
+import { useState } from "react";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 10px;
+  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const Box = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
+  min-height: 400px;
+  min-width: 400px;
+  height: 400px;
+  background-color: rgba(255, 255, 255, 0.7);
   border-radius: 40px;
   display: flex;
   justify-content: center;
   align-items: center;
-  overflow: hidden;
+  font-size: 28px;
+  font-weight: 600;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  cursor: pointer;
 `;
 
-const boxVars: Variants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Button = styled(motion.button)`
+  background-color: white;
+  color: black;
+  font-size: 30px;
+  font-weight: 600;
+  width: 100px;
+  height: 100px;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const overlayVars: Variants = {
+  initial: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  animate: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
 };
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
+  const [id, setId] = useState<null | string>(null);
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={1}
-          dragConstraints={biggerBoxRef}
-          variants={boxVars}
-          whileHover="hover"
-          whileTap="click"
-        ></Box>
-      </BiggerBox>
+      <Grid>
+        <AnimatePresence>
+          {["1", "2", "3", "4"].map((n) =>
+            n === "2" ? (
+              <Box
+                onClick={() => setId(n)}
+                key={n}
+                layoutId={n}
+                whileTap={{
+                  scale: 0.8,
+                  rotate: -90,
+                  borderRadius: "100%",
+                }}
+              >
+                {clicked ? <Circle layoutId="circle" style={{ borderRadius: 0 }} /> : null}
+              </Box>
+            ) : n === "3" ? (
+              <Box onClick={() => setId(n)} key={n} layoutId={n}>
+                {!clicked ? <Circle layoutId="circle" style={{ borderRadius: 50 }} /> : null}
+              </Box>
+            ) : n === "4" ? (
+              <Box
+                whileHover={{ translateX: 20, translateY: 20, scale: 1.1 }}
+                onClick={() => setId(n)}
+                key={n}
+                layoutId={n}
+              />
+            ) : (
+              <Box
+                whileHover={{ translateX: -20, translateY: -20, scale: 1.1 }}
+                onClick={() => setId(n)}
+                key={n}
+                layoutId={n}
+              />
+            )
+          )}
+        </AnimatePresence>
+      </Grid>
+      <Button
+        animate={clicked ? { scale: 1.2, color: "red" } : { scale: 1 }}
+        onClick={toggleClicked}
+      >
+        Click
+      </Button>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            onClick={() => setId(null)}
+            variants={overlayVars}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+          >
+            <Box layoutId={id} style={{ width: 200, height: 200 }} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
